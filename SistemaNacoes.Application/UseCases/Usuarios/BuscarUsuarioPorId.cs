0b497bea385b5518166dc0ed35ae744a31a5be19
BuildTools <tiago.sbittencourt@icloud.com>
2024-06-15 18:18:29 -0300

    feat: add get usuarios

---
+++ SistemaNacoes.Application/UseCases/Usuarios/BuscarUsuarioPorId.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using SistemaNacoes.Application.DTOs;
+using SistemaNacoes.Domain.Interfaces;
+using System.Runtime.CompilerServices;
+
+namespace SistemaNacoes.Application.UseCases.Usuarios
+{
+    public class BuscarUsuarioPorId
+    {
+        private readonly IUsuarioMinisterioRepository _usuarioMinisterioRepository;
+        private readonly IMapper _mapper;
+
+        public BuscarUsuarioPorId(IUsuarioMinisterioRepository usuarioMinisterioRepository,
+            IMapper mapper)
+        {
+            _usuarioMinisterioRepository = usuarioMinisterioRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<LerUsuarioDTO> ExecuteAsync(int id)
+        {
+            var usuario = await _usuarioMinisterioRepository.GetByIdAsync(id);
+
+            return usuario != null ? _mapper.Map<LerUsuarioDTO>(usuario) : throw new Exception("Usuário não encontrado.");
+        }
+    }
+}
---
+++ SistemaNacoes.WebAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaNacoes.Application.DTOs;
+using SistemaNacoes.Application.UseCases.Usuarios;
+
+namespace SistemaNacoes.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly CriarUsuario _criarUsuario;
+        private readonly BuscarUsuarioPorId _buscarUsuarioPorId;
+
+        public UsuariosController(CriarUsuario criarUsuario,
+            BuscarUsuarioPorId buscarUsuarioPorId)
+        {
+            _criarUsuario = criarUsuario;
+            _buscarUsuarioPorId = buscarUsuarioPorId;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CriarUsuarioDTO dto)
+        {
+            var usuario = await _criarUsuario.ExecuteAsync(dto);
+            return CreatedAtAction(nameof(Get), new { id = usuario.Id }, usuario);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var usuario = await _buscarUsuarioPorId.ExecuteAsync(id);
+            return Ok(usuario);
+        }
+    }
+}
