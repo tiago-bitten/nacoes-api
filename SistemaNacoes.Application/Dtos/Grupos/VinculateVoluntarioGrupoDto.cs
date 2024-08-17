a87a2903bdd84b02935f3538a4553ec06ae66d0a
BuildTools <tiago.sbittencourt@icloud.com>
2024-08-17 20:02:04 -0300

    feat: add

---
+++ SistemaNacoes.Application/Dtos/Grupos/VinculateVoluntarioGrupoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SistemaNacoes.Application.Dtos.Grupos;
+
+public class VinculateVoluntarioGrupoDto
+{
+    public int GrupoId { get; set; }
+    public int VoluntarioId { get; set; }
+}
---
+++ SistemaNacoes.Application/UseCases/Grupos/VinculateVoluntarioGrupo.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using SistemaNacoes.Application.Dtos.Grupos;
+using SistemaNacoes.Application.Responses;
+using SistemaNacoes.Domain.Interfaces.Repositorios;
+using SistemaNacoes.Domain.Interfaces.Services;
+
+namespace SistemaNacoes.Application.UseCases.Grupos;
+
+public class VinculateVoluntarioGrupo
+{
+    private readonly IUnitOfWork _uow;
+    private readonly IMapper _mapper;
+    private readonly IVoluntarioService _voluntarioService;
+    
+
+    public VinculateVoluntarioGrupo(IUnitOfWork uow, IMapper mapper, IVoluntarioService voluntarioService)
+    {
+        _uow = uow;
+        _mapper = mapper;
+        _voluntarioService = voluntarioService;
+    }
+
+    public async Task<RespostaBase<dynamic>> ExecuteAsync(VinculateVoluntarioGrupoDto dto)
+    {
+        
+    }
+}
