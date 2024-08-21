0c773dd5144f890e94788059b5baa043535d8aef
BuildTools <tiago.sbittencourt@icloud.com>
2024-08-21 20:23:45 -0300

    feat: add get all agendamentos includes

--- SistemaNacoes.Application/UseCases/Agendamentos/GetAllAgendamentos.cs
+++ SistemaNacoes.Application/UseCases/Agendamentos/GetAllAgendamentos.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using SistemaNacoes.Application.Dtos.Agendamentos;
 using SistemaNacoes.Application.Responses;
+using SistemaNacoes.Domain.Entidades;
 using SistemaNacoes.Domain.Interfaces.Repositorios;
 
 namespace SistemaNacoes.Application.UseCases.Agendamentos;
@@ -19,12 +20,19 @@ public class GetAllAgendamentos
     
     public async Task<RespostaBase<List<GetAgendamentoDto>>> ExecuteAsync(QueryParametro query)
     {
+        var includes = new[]
+        {
+            nameof(Agendamento.Voluntario),
+            nameof(Agendamento.Ministerio),
+            $"{nameof(Agendamento.AgendamentoAtividades)}.{nameof(AgendamentoAtividade.Atividade)}"
+        };
+        
         var totalAgendamentos = await _uow.Agendamentos
-            .GetAll()
+            .GetAll(includes)
             .CountAsync(x => !x.Removido);
         
         var agendamentos = await _uow.Agendamentos
-            .GetAll()
+            .GetAll(includes)
             .Where(x => !x.Removido)
             .Skip(query.Skip)
             .Take(query.Take)
