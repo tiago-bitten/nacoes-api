56f01ab5faeb90d5a52854e357a19d9b9437c3ac
BuildTools <tiago.sbittencourt@icloud.com>
2024-08-23 21:32:50 -0300

    feat: add auth controller

---
+++ SistemaNacoes.API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaNacoes.Application.Dtos.Auth;
+using SistemaNacoes.Application.UseCases.Auth;
+
+namespace SistemaNacoes.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly RefreshToken _refreshToken;
+
+    public AuthController(RefreshToken refreshToken)
+    {
+        _refreshToken = refreshToken;
+    }
+    
+    [HttpPost("RefreshToken")]
+    public async Task<IActionResult> RefreshToken([FromBody] RefreshAuthTokenDto dto)
+    {
+        var result = await _refreshToken.ExecuteAsync(dto);
+        
+        return Ok(result);
+    }
+}
