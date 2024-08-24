357e9f818b5a4c1d65b240addc23eebb05307043
BuildTools <tiago.sbittencourt@icloud.com>
2024-08-23 21:33:47 -0300

    keep login imp

---
+++ SistemaNacoes.Application/UseCases/Auth/Login.cs
@@ -0,0 +1,13 @@
+﻿using SistemaNacoes.Domain.Interfaces.Repositorios;
+
+namespace SistemaNacoes.Application.UseCases.Auth;
+
+public class Login
+{
+    private readonly IUnitOfWork _uow;
+
+    public Login(IUnitOfWork uow)
+    {
+        _uow = uow;
+    }
+}
