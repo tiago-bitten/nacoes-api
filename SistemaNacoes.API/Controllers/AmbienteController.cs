6c60462655a757c9ad809f9c5fdbc1c103ecc48f
BuildTools <tiago.sbittencourt@icloud.com>
2024-08-26 23:40:54 -0300

    feat: add ambiente usuario controller

---
+++ SistemaNacoes.API/Controllers/AmbienteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaNacoes.Application.UseCases.Permissoes;
+
+namespace SistemaNacoes.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AmbienteController : ControllerBase
+{
+    private readonly GetUsuarioPermissoes _getUsuarioPermissoes;
+
+    public AmbienteController(GetUsuarioPermissoes getUsuarioPermissoes)
+    {
+        _getUsuarioPermissoes = getUsuarioPermissoes;
+    }
+
+    public async Task<IActionResult> GetPermissoesUsuario()
+    {
+        var result = await _getUsuarioPermissoes.ExecuteAsync();
+        
+        return Ok(result);
+    }
+}
