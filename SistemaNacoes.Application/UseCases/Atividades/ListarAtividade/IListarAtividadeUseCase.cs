78eae1728b9a390c00ca94369b97e3f31f5e1e4e
BuildTools <tiago.sbittencourt@icloud.com>
2024-10-17 21:00:03 -0300

    listar atividades

---
+++ SistemaNacoes.Application/UseCases/Atividades/ListarAtividade/IListarAtividadeUseCase.cs
@@ -0,0 +1,8 @@
+﻿using SistemaNacoes.Application.UseCases.Atividades.ListarAtividade.Dtos;
+
+namespace SistemaNacoes.Application.UseCases.Atividades.ListarAtividade;
+
+public interface IListarAtividadeUseCase : IListarUseCaseBase<ListarAtividadeResult, ListarAtividadeParams>
+{
+    
+}
