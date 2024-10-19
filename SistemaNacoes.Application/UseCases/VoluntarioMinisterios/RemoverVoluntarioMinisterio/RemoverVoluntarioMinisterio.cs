161f1b62e705703747a44e9983a6a0c6477909df
BuildTools <tiago.sbittencourt@icloud.com>
2024-10-18 21:01:35 -0300

    remove voluntario ministerio

---
+++ SistemaNacoes.Application/UseCases/VoluntarioMinisterios/RemoverVoluntarioMinisterio/RemoverVoluntarioMinisterio.cs
@@ -0,0 +1,34 @@
+﻿using SistemaNacoes.Domain.Enums;
+using SistemaNacoes.Domain.Interfaces.Repositorios;
+using SistemaNacoes.Domain.Interfaces.Services;
+
+namespace SistemaNacoes.Application.UseCases.VoluntarioMinisterios.RemoverVoluntarioMinisterio;
+
+public class RemoverVoluntarioMinisterio : IRemoverVoluntarioMinisterioUseCase
+{
+    #region Ctor
+    private readonly IVoluntarioMinisterioService _service;
+    private readonly IUnitOfWork _uow;
+    private readonly IPermissoesService _permissoesService;
+    private readonly IHistoricoEntidadeService _historicoService;
+
+    public RemoverVoluntarioMinisterio(IVoluntarioMinisterioService service, IUnitOfWork uow, IPermissoesService permissoesService, IHistoricoEntidadeService historicoService)
+    {
+        _service = service;
+        _uow = uow;
+        _permissoesService = permissoesService;
+        _historicoService = historicoService;
+    }
+    #endregion
+
+    public async Task ExecutarAsync(int id)
+    {
+        await _permissoesService.VerificaGarantePermissaoAsync(EPermissoes.DELETE_VOLUNTARIO_MINISTERIO, "Você não tem permissão para desvincular o voluntário ao ministério.");
+        
+        var voluntarioMinisterio = await _service.RecuperaGaranteExisteAsync(id);
+        
+        await _uow.IniciarTransacaoAsync();
+        _service.Remover(voluntarioMinisterio);
+        await _uow.CommitTransacaoAsync();
+    }
+}
