42a54584e25cc3403c2c068e26e0e63236cb4c7b
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-10-28 07:21:18 -0300

    controoler

--- SistemaNacoes.API/Controllers/AgendamentosController.cs
+++ SistemaNacoes.API/Controllers/AgendamentosController.cs
@@ -2,6 +2,7 @@
 using SistemaNacoes.Application.UseCases;
 using SistemaNacoes.Application.UseCases.Agendamentos.CriarAgendamento;
 using SistemaNacoes.Application.UseCases.Agendamentos.CriarAgendamento.Dtos;
+using SistemaNacoes.Application.UseCases.Agendamentos.RemoverAgendamento;
 
 namespace SistemaNacoes.API.Controllers;
 
@@ -11,17 +12,33 @@ public class AgendamentosController : ControllerBase
 {
     #region Ctor
     private readonly ICriarAgendamentoUseCase _criar;
+    private readonly IRemoverAgendamentoUseCase _remover;
 
-    public AgendamentosController(ICriarAgendamentoUseCase criar)
+    public AgendamentosController(
+        ICriarAgendamentoUseCase criar, 
+        IRemoverAgendamentoUseCase remover)
     {
         _criar = criar;
+        _remover = remover;
     }
     #endregion
 
+    #region Criar
     [HttpPost]
     public async Task<IActionResult> Criar(CriarAgendamentoRequest request)
     {
         var result = await _criar.ExecutarAsync(request);
         return Ok(result);
     }
+    #endregion
+    
+    #region Remover
+    [HttpDelete("{id}")]
+    [HttpDelete("Remover/{id}")]
+    public async Task<IActionResult> Remover(int id)
+    {
+        await _remover.ExecutarAsync(id);
+        return Ok();
+    }
+    #endregion
 }
--- SistemaNacoes.API/Controllers/AgendasController.cs
+++ SistemaNacoes.API/Controllers/AgendasController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaNacoes.Application.UseCases.Agendas.AbrirAgenda;
+using SistemaNacoes.Application.UseCases.Agendas.AbrirAgenda.Dtos;
+using SistemaNacoes.Application.UseCases.Agendas.ConcluirAgenda;
+using SistemaNacoes.Application.UseCases.Agendas.ListarAgenda;
+using SistemaNacoes.Application.UseCases.Agendas.RemoverAgenda;
 
 namespace SistemaNacoes.API.Controllers;
 
@@ -6,4 +11,27 @@ namespace SistemaNacoes.API.Controllers;
 [ApiController]
 public class AgendasController : ControllerBase
 {
+    #region Ctor
+    private readonly IAbrirAgendaUseCase _abrir;
+    private readonly IRemoverAgendaUseCase _remover;
+    private readonly IConcluirAgendaUseCase _concluir;
+    private readonly IListarAgendaUseCase _listar;
+
+    public AgendasController(IAbrirAgendaUseCase abrir, IRemoverAgendaUseCase remover, IConcluirAgendaUseCase concluir, IListarAgendaUseCase listar)
+    {
+        _abrir = abrir;
+        _remover = remover;
+        _concluir = concluir;
+        _listar = listar;
+    }
+    #endregion
+
+    #region Abrir
+    [HttpPost("Abrir")]
+    public async Task<IActionResult> Abrir([FromBody] AbrirAgendaRequest request)
+    {
+        var result = await _abrir.ExecutarAsync(request);
+        return Ok();
+    }
+    #endregion
 }
