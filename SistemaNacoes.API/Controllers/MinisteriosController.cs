0ff516496192a5339b3e4737e09ee2ecd40ad428
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-11-01 22:43:23 -0300

    ministerios

--- SistemaNacoes.API/Controllers/MinisteriosController.cs
+++ SistemaNacoes.API/Controllers/MinisteriosController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaNacoes.Application.UseCases.Ministerios.CriarMinisterio;
+using SistemaNacoes.Application.UseCases.Ministerios.CriarMinisterio.Dtos;
+using SistemaNacoes.Application.UseCases.Ministerios.RemoverMinisterio;
 
 namespace SistemaNacoes.API.Controllers;
 
@@ -6,4 +9,35 @@ namespace SistemaNacoes.API.Controllers;
 [Route("api/[controller]")]
 public class MinisteriosController : ControllerBase
 {
+    #region Ctor
+    private readonly ICriarMinisterioUseCase _criar;
+    private readonly IRemoverMinisterioUseCase _remover;
+    
+    public MinisteriosController(
+        ICriarMinisterioUseCase criar,
+        IRemoverMinisterioUseCase remover)
+    {
+        _criar = criar;
+        _remover = remover;
+    }
+    #endregion
+    
+    #region Criar
+    [HttpPost]
+    [HttpPost("Criar")]
+    public async Task<IActionResult> Criar([FromBody] CriarMinisterioRequest request)
+    {
+        var result = await _criar.ExecutarAsync(request);
+        return Ok();
+    } 
+    #endregion
+    
+    #region Remover
+    [HttpDelete("Remover/{id:int}")]
+    public async Task<IActionResult> Remover(int id)
+    {
+        await _remover.ExecutarAsync(id);
+        return Ok();
+    }
+    #endregion
 }
