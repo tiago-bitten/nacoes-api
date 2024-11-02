63c25f3f08cb5d31728b7bbd5109f3f73bd07a59
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-11-01 22:33:35 -0300

    data indisponivel

--- SistemaNacoes.API/Controllers/DataIndisponiveisController.cs
+++ SistemaNacoes.API/Controllers/DataIndisponiveisController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaNacoes.Application.UseCases.DataIndisponiveis.CriarDataIndisponivel;
+using SistemaNacoes.Application.UseCases.DataIndisponiveis.CriarDataIndisponivel.Dtos;
+using SistemaNacoes.Application.UseCases.DataIndisponiveis.RemoverDataIndisponivel;
 
 namespace SistemaNacoes.API.Controllers;
 
@@ -6,4 +9,36 @@ namespace SistemaNacoes.API.Controllers;
 [Route("api/[controller]")]
 public class DataIndisponiveisController : ControllerBase
 {
+    #region Ctor
+    private readonly ICriarDataIndisponivelUseCase _criar;
+    private readonly IRemoverDataIndisponivelUseCase _remover;
+    
+    public DataIndisponiveisController(
+        ICriarDataIndisponivelUseCase criar,
+        IRemoverDataIndisponivelUseCase remover)
+    {
+        _criar = criar;
+        _remover = remover;
+    }
+    #endregion
+    
+    #region Criar
+    [HttpPost]
+    [HttpPost("Criar")]
+    public async Task<IActionResult> Criar([FromBody] CriarDataIndisponivelRequest request)
+    {
+        var result = await _criar.ExecutarAsync(request);
+        return Ok();
+    }
+    #endregion
+    
+    #region Remover
+
+    [HttpDelete("Remover/{id:int}")]
+    public async Task<IActionResult> Remover(int id)
+    {
+        await _remover.ExecutarAsync(id);
+        return Ok();
+    }
+    #endregion
 }
