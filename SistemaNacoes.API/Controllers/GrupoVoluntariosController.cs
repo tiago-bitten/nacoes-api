e35f793b15aab770f3e5693810f8b31500e41cd8
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-11-01 22:39:57 -0300

    grupo voluntario

--- SistemaNacoes.API/Controllers/GrupoVoluntariosController.cs
+++ SistemaNacoes.API/Controllers/GrupoVoluntariosController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaNacoes.Application.UseCases.GrupoVoluntarios.CriarGrupoVoluntario;
+using SistemaNacoes.Application.UseCases.GrupoVoluntarios.CriarGrupoVoluntario.Dtos;
 
 namespace SistemaNacoes.API.Controllers;
 
@@ -6,4 +8,22 @@ namespace SistemaNacoes.API.Controllers;
 [Route("api/[controller]")]
 public class GrupoVoluntariosController : ControllerBase
 {
+    #region Ctor
+    private readonly ICriarGrupoVoluntarioUseCase _criar;
+    
+    public GrupoVoluntariosController(ICriarGrupoVoluntarioUseCase criar)
+    {
+        _criar = criar;
+    }
+    #endregion
+    
+    #region Criar
+    [HttpPost]
+    [HttpPost("Criar")]
+    public async Task<IActionResult> Criar([FromBody] CriarGrupoVoluntarioRequest request)
+    {
+        var result = await _criar.ExecutarAsync(request);
+        return Ok();
+    }
+    #endregion
 }
