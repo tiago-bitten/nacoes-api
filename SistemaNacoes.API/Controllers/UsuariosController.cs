7b4d9444ab6bfee551eba8d4e66b392044eb9653
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-11-02 21:24:30 -0300

    criar

--- SistemaNacoes.API/Controllers/UsuariosController.cs
+++ SistemaNacoes.API/Controllers/UsuariosController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaNacoes.Application.UseCases.Usuarios.CriarUsuario;
+using SistemaNacoes.Application.UseCases.Usuarios.CriarUsuario.Dtos;
 
 namespace SistemaNacoes.API.Controllers;
 
@@ -6,4 +8,22 @@ namespace SistemaNacoes.API.Controllers;
 [Route("api/[controller]")]
 public class UsuariosController : ControllerBase
 {
+    #region Ctor
+    private readonly ICriarUsuarioUseCase _criar;
+    
+    public UsuariosController(ICriarUsuarioUseCase criar)
+    {
+        _criar = criar;
+    }
+    #endregion
+    
+    #region Criar
+    [HttpPost]
+    [HttpPost("Criar")]
+    public async Task<IActionResult> Criar([FromBody] CriarUsuarioRequest request)
+    {
+        var result = await _criar.ExecutarAsync(request);
+        return Ok();
+    }
+    #endregion
 }
