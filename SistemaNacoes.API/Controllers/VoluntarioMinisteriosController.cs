f32e7d0ba51e8b6ba52ab6292c06610f612fa49f
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-11-02 21:26:26 -0300

    usu

--- SistemaNacoes.API/Controllers/VoluntarioMinisteriosController.cs
+++ SistemaNacoes.API/Controllers/VoluntarioMinisteriosController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaNacoes.Application.UseCases.VoluntarioMinisterios.CriarVoluntarioMinisterio;
+using SistemaNacoes.Application.UseCases.VoluntarioMinisterios.CriarVoluntarioMinisterio.Dtos;
+using SistemaNacoes.Application.UseCases.VoluntarioMinisterios.ListarVoluntarioMinisterio;
+using SistemaNacoes.Application.UseCases.VoluntarioMinisterios.RemoverVoluntarioMinisterio;
 
 namespace SistemaNacoes.API.Controllers;
 
@@ -6,4 +10,31 @@ namespace SistemaNacoes.API.Controllers;
 [Route("api/[controller]")]
 public class VoluntarioMinisteriosController : ControllerBase
 {
+    #region Ctor
+    private readonly ICriarVoluntarioMinisterioUseCase _criar;
+    private readonly IRemoverVoluntarioMinisterioUseCase _remover;
+    private readonly IListarVoluntarioMinisterioUseCase _listar;
+    
+    public VoluntarioMinisteriosController(
+        ICriarVoluntarioMinisterioUseCase criar,
+        IRemoverVoluntarioMinisterioUseCase remover,
+        IListarVoluntarioMinisterioUseCase listar)
+    {
+        _criar = criar;
+        _remover = remover;
+        _listar = listar;
+    }
+
+    #endregion
+    
+    #region Criar
+
+    [HttpPost]
+    [HttpPost("Criar")]
+    public async Task<IActionResult> Criar([FromBody] CriarVoluntarioMinisterioRequest request)
+    {
+        var result = await _criar.ExecutarAsync(request);
+        return Ok();
+    }
+    #endregion
 }
